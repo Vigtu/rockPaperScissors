90c4617d1c1bcf3dc6bc7681d1169f65e26e91f9
VICTOR CORREA GOMES <112295415+Vigtu@users.noreply.github.com>
2022-09-24 19:57:35 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,65 @@
+import random
+import os
+
+listaBot = ['PAPEL', 'TESOURA', 'PEDRA']
+
+#contador
+contagem = 0
+contagemBot = 0
+print('Bem vindo ao jogo Pedra, Papel e Tesoura!')
+def placar():
+    print('-------------------------------------------')
+    print('Placar:\nBot:{}\nVocê: {}'.format(contagemBot, contagem))
+    print('-------------------------------------------')
+    
+
+def bot():
+    escolhaBot = random.choice(listaBot)
+    print(f'O bot escolheu:{escolhaBot}')
+    escolha = input('Escolha o seu lance:\n 1 - Papel | 2 - Pedra | 3 - Tesoura\n')
+
+    #derrotas
+    if escolha == '1' and escolhaBot == 'TESOURA':
+        global contagemBot
+        contagemBot += 1
+        print('Você Perdeu')
+    
+    if escolha == '2' and escolhaBot == 'PAPEL':
+        print('Você Perdeu')
+        contagemBot += 1
+
+    if escolha == '3' and escolhaBot == 'PEDRA':
+        print('Você Perdeu')
+        contagemBot += 1
+
+    #vitórias
+    if escolha == '1' and escolhaBot == 'PEDRA':
+        print('Você Ganhou')
+        global contagem
+        contagem += 1
+
+    if escolha == '2' and escolhaBot == 'TESOURA':
+        print('Você Ganhou')
+        contagem += 1
+        
+    if escolha == '3' and escolhaBot == 'PAPEL':
+        print('Você Ganhou')
+        contagem += 1
+
+    #empate
+    else:
+        print('Empate!')
+
+execucao = True
+
+while execucao:
+    placar()
+    bot()
+    reset = str(input('Deseja jogar novamente?:\n[S/N]\n')).upper().strip()
+    if reset == 'N':
+        execucao = False
+        exit('Obrigado por jogar!')
+    else:
+         os.system('cls')
+
+
